ca2a3a7909d8c4da03bfcc27c1bb68b4b9607f9a
CarlosHeiden <carlos.heiden@outlook.com>
2023-10-01 15:43:34 -0300

    revisao operadores matematicos

---
+++ revisao/modulo02/02/main.py
@@ -0,0 +1,35 @@
+from controller import create, read, search, update, delete
+
+
+def menu():
+    cond = 'sim'
+    while cond == 'sim':
+        var = int(input('1 - Criar\n2 - Listar\n3 - Pesquisar\n4 - Alterar\n5 - Deletar\n>>'))
+        match(var):
+            case 1:
+                pessoa = {}
+                pessoa['Nome']      = input('Digite seu nome:')
+                pessoa['cpf']       = input('Digite seu cpf:')
+                pessoa['idade']     = int(input('Digite sua idade:'))
+                pessoa['telefone']  = input('Digite seu telefone:')
+                create(pessoa)
+            case 2:
+                print(read())
+            case 3:
+                pass
+            case 4:
+                update(input('Digite o nome da pessoa que deseja alterar: '))
+            case 5:
+                pessoa = (input('Informe o cliente que deseja deletar: '))
+                delete(pessoa)
+        cond = input('Deseja continuar\nSim\nNão')
+
+menu()
+
+
+
+
+# create(input('digite seu nome: '))
+# print(read())
+# search(input('Digite o nome que deseja pesquisar:'))
+# update(input('Digite a pessoa que deseja atualizar: '))
